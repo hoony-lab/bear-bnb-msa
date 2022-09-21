597de43eac7a7735ac4c4e976aa1e57fbf55efc5
Hoony <jihun9838@gmail.com>
2022-09-21 08:30:43 +0000

    update gateway

--- gateway/src/main/java/com/example/template/Application.java
+++ gateway/src/main/java/com/example/template/Application.java
@@ -1,11 +1,9 @@
 package com.example.template;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
 
     public static ApplicationContext applicationContext;
